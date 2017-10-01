fa89811b02705ceb53ba9c23e11d8a84c0997435
au <alexeyu@aspentechlabs.com>
2017-10-01 13:45:57 +0300

    a

--- train.py
+++ train.py
@@ -8,15 +8,15 @@ K.set_image_data_format("channels_first")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--save_weights_path", type = str  )
-parser.add_argument("--train_images", type = str  )
-parser.add_argument("--train_annotations", type = str  )
+parser.add_argument("--train_images", type = str, default= "/home/au/pro/imgdtct/training/image-segmentation-keras/data/dataset1/images_prepped_train/" )
+parser.add_argument("--train_annotations", type = str, default = "/home/au/pro/imgdtct/training/image-segmentation-keras/data/dataset1/annotations_prepped_train/" )
 parser.add_argument("--n_classes", type=int, default = 10 )
-parser.add_argument("--input_height", type=int , default = 224  )
+parser.add_argument("--input_height", type=int , default = 224 )
 parser.add_argument("--input_width", type=int , default = 224 )
 
 parser.add_argument('--validate',action='store_false')
-parser.add_argument("--val_images", type = str , default = "")
-parser.add_argument("--val_annotations", type = str , default = "")
+parser.add_argument("--val_images", type = str , default = "/home/au/pro/imgdtct/training/image-segmentation-keras/data/dataset1/images_prepped_test/")
+parser.add_argument("--val_annotations", type = str , default = "/home/au/pro/imgdtct/training/image-segmentation-keras/data/dataset1/annotations_prepped_test/")
 
 parser.add_argument("--epochs", type = int, default = 5 )
 parser.add_argument("--batch_size", type = int, default = 2 )
@@ -73,12 +73,12 @@ if validate:
 
 if not validate:
 	for ep in range( epochs ):
-		m.fit_generator( G , 512  , epochs=1 )
+		m.fit_generator( G , 512  , epochs = 1 )
 		m.save_weights( save_weights_path + "." + str( ep ) )
 		m.save( save_weights_path + ".model." + str( ep ) )
 else:
 	for ep in range( epochs ):
-		m.fit_generator( G , 512  , validation_data=G2 , validation_steps=200 ,  epochs=1 )
+		m.fit_generator( G , 512  , validation_data=G2 , validation_steps=200 ,  epochs = 1 )
 		m.save_weights( save_weights_path + "." + str( ep )  )
 		m.save( save_weights_path + ".model." + str( ep ) )
 
